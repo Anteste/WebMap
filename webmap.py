12ccddd9b5103599251e8706b0dcb883f3773342
Iliass Alami Qammouri <ialami2006@gmail.com>
2020-12-29 13:35:50 +0000

    Network Scan Fix

--- webmap.py
+++ webmap.py
@@ -17,71 +17,71 @@ while ans:
     print("E. Exit")
     print('====================================')
     ans=input("What would you like to do? Enter your selection: ")
+    
     if ans=="1":
       os.system('cls' if os.name == 'nt' else 'clear')
+      ans=True
+      while ans:
+          print
+          print('====================================')
+          print('#            Nmap Scan          #')
+          print('====================================')
+          print("1. Scan An IP Address For Open Ports")
+          print("2. Operating System Scan")
+          print("3. Agressive Scan For An IP Address")
+          print("4. Scan The Network For All Devices")
+          print("M. Main Menu")
+          print('====================================')
+          print 
+          ans=input("What would you like to do? Enter your selection: ")
 
-    ans=True
-    while ans:
-        print
-        print('====================================')
-        print('#            Network Time          #')
-        print('====================================')
-        print("1. Scan An IP Address For Open Ports")
-        print("2. Operating System Scan")
-        print("3. Agressive Scan For An IP Address")
-        print("4. Scan The Network For All Devices")
-        print("M. Main Menu")
-        print('====================================')
-        print 
-        ans=input("What would you like to do? Enter your selection: ")
-
-        if ans=="1":
-              os.system('cls' if os.name == 'nt' else 'clear')
-              print
-              print("______________________________________________________________________")
-              print
-              portscan=input("Enter the IP you want to scan the ports of: ")
-              print("______________________________________________________________________")
-              os.system("sudo nmap " + portscan)
-              print("______________________________________________________________________")
-        elif ans=="2":
-              os.system('cls' if os.name == 'nt' else 'clear')
-              print
-              print("______________________________________________________________________")
-              print
-              osscan=input("Enter the IP you want to find the operating system of: ")
-              print("______________________________________________________________________")
-              os.system("sudo nmap -O " + osscan)
-              print("______________________________________________________________________") 
-                
-        elif ans=="3":
+          if ans=="1":
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print
                 print("______________________________________________________________________")
                 print
-                ascan=input("Enter the IP you want to scan: ")
-                print
+                portscan=input("Enter the IP you want to scan the ports of: ")
                 print("______________________________________________________________________")
-                os.system("sudo nmap -A " + address)
+                os.system("sudo nmap " + portscan)
                 print("______________________________________________________________________")
-        elif ans=="4":
-              os.system('cls' if os.name == 'nt' else 'clear')
-              print
-              print("______________________________________________________________________")
-              print
-              address=input("Enter your address and range (i.e. 192.168.0.1/24) now: ")
-              print
-              print("______________________________________________________________________")
-              os.system("sudo nmap -sn " + address)
-              print("______________________________________________________________________")
-        elif ans=="M":
-                os.system('cls' if os.name == 'nt' else 'clear')
-                os.system('python3 webmap.py')
-                
-        else:
+          elif ans=="2":
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print
-                print("Not Valid Choice Try again")
+                print("______________________________________________________________________")
                 print
-                os.system("python3 webmap.py")
-                ans = None
+                osscan=input("Enter the IP you want to find the operating system of: ")
+                print("______________________________________________________________________")
+                os.system("sudo nmap -O " + osscan)
+                print("______________________________________________________________________") 
+                  
+          elif ans=="3":
+                  os.system('cls' if os.name == 'nt' else 'clear')
+                  print
+                  print("______________________________________________________________________")
+                  print
+                  ascan=input("Enter the IP you want to scan: ")
+                  print
+                  print("______________________________________________________________________")
+                  os.system("sudo nmap -A " + address)
+                  print("______________________________________________________________________")
+          elif ans=="4":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print
+                print("______________________________________________________________________")
+                print
+                address=input("Enter your address and range (i.e. 192.168.0.1/24) now: ")
+                print
+                print("______________________________________________________________________")
+                os.system("sudo nmap -sn " + address)
+                print("______________________________________________________________________")
+          elif ans=="M":
+                  os.system('cls' if os.name == 'nt' else 'clear')
+                  os.system('python3 webmap.py')
+                  
+          else:
+                  os.system('cls' if os.name == 'nt' else 'clear')
+                  print
+                  print("Not Valid Choice Try again")
+                  print
+                  os.system("python3 webmap.py")
+                  ans = None
