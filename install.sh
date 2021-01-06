bbe20330a27b026a00db7622c1a6ba38cf28ec65
Anteste <ialami2006@gmail.com>
2021-01-06 06:19:30 +0000

    Fix some bugs

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-get install gnome-terminal -y
 sudo apt-get install nmap -y
 sudo apt-get install nikto -y
-cd /opt && sudo git clone https://github.com/maurosoria/dirsearch.git
 sudo chmod +x webmap.py
 sudo cp webmap.py /bin/webmap
-
+cd /opt && sudo git clone https://github.com/maurosoria/dirsearch.git
+cd -
